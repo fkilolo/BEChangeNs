6b6766b579dcfa3157edf620fc14ef97c4bdbe9a
osnezreal <osnezreal1@okvip.com>
2025-07-21 22:29:24 +0700

    update CRUD conect spaceship

---
+++ src/modules/spaceship/dto/create-spaceship.dto.ts
@@ -0,0 +1,7 @@
+export class CreateSpaceshipDto {
+  name: string;
+  apikey: string;
+  secretkey: string;
+  total_domain: number;
+  userName: string;
+} 
---
+++ src/modules/spaceship/schemas/spaceship.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Spaceship extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  apikey: string;
+
+  @Prop({ required: true })
+  secretkey: string;
+
+  @Prop({ required: true, default: 0 })
+  total_domain: number;
+
+  @Prop({ required: true })
+  userName: string;
+}
+
+export const SpaceshipSchema = SchemaFactory.createForClass(Spaceship); 

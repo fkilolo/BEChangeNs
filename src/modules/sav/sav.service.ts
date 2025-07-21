e3f46ca3bb8a3a64bb482bc3c5f1932eee147ff1
tuudeptrai <vuvantuu.gl@gmail.com>
2025-07-21 11:33:01 +0700

    SAV_API_KEY=fqNKjEPVxod4VhZdtA4voDTQgmDLW5P41jCSRxnH

--- src/modules/sav/sav.service.ts
+++ src/modules/sav/sav.service.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const SAV_API_BASE = 'https://api.sav.com/domains_api_v1';
-const APIKEY = process.env.SAV_API_KEY || 'demo-api-key';
+const APIKEY = process.env.SAV_API_KEY || 'fqNKjEPVxod4VhZdtA4voDTQgmDLW5P41jCSRxnH';
 
 const savHeaders = {
   headers: {
